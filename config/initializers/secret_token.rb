d954ebf11e210d08700ab733cad0a62c5ddc9f3d
Shridhar Deshmukh <shridhad@thoughtworks.com>
2013-03-04 23:08:31 +0530

    #init - Shridhar - Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JDo::Application.config.secret_token = '02e400ef2c77f1839e670ff91eea7b42e2689ffc86579a6c2d7ce461d146594c0eacf964cfafe90a3e11b7bd93d1c50af051101859c4613ad86202c29fb35542'
